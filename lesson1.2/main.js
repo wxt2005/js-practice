1b71d177f3264e41f321b7b0a4a13a7487864cdf
Botao <wxt2005@gmail.com>
2014-05-29 15:11:45 +0800

    lesson1.2 main js added

---
+++ lesson1.2/main.js
@@ -0,0 +1,21 @@
+(function() {
+    var skin = document.getElementById("skin");
+    var buttons = skin.getElementsByTagName("li");
+    var link = document.getElementById("skinlink");
+
+    var buttonListener = function(event) {
+        if (event.target.tagName.toLowerCase() === "li") {
+            for (var i = 0, l = buttons.length; i < l; i++) {
+                console.log(buttons[i]);
+                if (buttons[i] === event.target) {
+                    buttons[i].className = "current";
+                    link.href = "css\/" + event.target.id + ".css";  
+                } else {
+                    buttons[i].className = "";
+                }
+            }
+        }
+    };
+
+    skin.addEventListener("click", buttonListener, false);
+})();
